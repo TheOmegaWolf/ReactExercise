57bb3e44b4876f884438229986e2946b24f88ab3
Akshay Prassanna <50097410+TheOmegaWolf@users.noreply.github.com>
2022-05-25 02:09:20 +0530

    module update fix

--- src/Components/Avatar/Avatar.js
+++ src/Components/Avatar/Avatar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import style from "./Avatar.module.css"
 
-export default function ({img}) {
+export default function Avatar({img}) {
   return (
     <>
         <img src={(img)} width={"250px"} height={"250px"} className={style.imageContainer} alt="DisplayPic"/>
